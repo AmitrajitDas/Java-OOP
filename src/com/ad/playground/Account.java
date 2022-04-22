78599eb5660eb98c8c646bc617e26617d575120b
AmitrajitDas <amitrajitdas31@gmail.com>
2022-04-22 18:05:29 +0530

    new: Vip Person

--- src/com/ad/playground/Account.java
+++ src/com/ad/playground/Account.java
@@ -28,7 +28,7 @@ public class Account {
     }
 
     public void withdrawal(double withdrawalAmount) {
-        if (this.balance - withdrawalAmount <= 0) {
+        if (this.balance - withdrawalAmount < 0) {
             System.out.println("Only " + this.balance + " available. Withdrawal not processed");
         } else {
             this.balance -= withdrawalAmount;
--- src/com/ad/playground/Main.java
+++ src/com/ad/playground/Main.java
@@ -17,5 +17,15 @@ public class Main {
 
         bobsAccount.deposit(51.0);
         bobsAccount.withdrawal(100.0);
+
+        VipPerson person1 = new VipPerson();
+        System.out.println(person1.getName());
+
+        VipPerson person2 = new VipPerson("Bob", 25000.00);
+        System.out.println(person2.getName());
+
+        VipPerson person3 = new VipPerson("Tim", 100.00, "tim@email.com");
+        System.out.println(person3.getName());
+        System.out.println(person3.getEmailAddress());
     }
 }
