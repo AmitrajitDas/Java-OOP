310cbdf655ba3d1e25931180121e0f4e0d046a99
AmitrajitDas <amitrajitdas31@gmail.com>
2022-04-27 21:41:10 +0530

    static blocks

--- src/com/ad/practiceStatic/OuterClass.java
+++ src/com/ad/practiceStatic/OuterClass.java
@@ -4,9 +4,17 @@ public class OuterClass {
 
     static class InnerClass {
         static String name;
-//        String name;
+        //        String name;
+        static int a = 4;
+        static int b;
+
         InnerClass(String name) {
             this.name = name;
+        }
+
+        static {
+            b = a * 6;
+            System.out.println("static block");
         }
     }
 
@@ -16,6 +24,9 @@ public class OuterClass {
 
         System.out.println(InnerClass.name);
         System.out.println(InnerClass.name);
+        System.out.println(InnerClass.a);
+        System.out.println(InnerClass.b);
+        System.out.println(InnerClass.b);
 
 //        System.out.println(a.name);
 //        System.out.println(b.name);
